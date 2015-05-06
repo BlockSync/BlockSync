45f786379d9197de6181b7603230b4cbb3c99022
Ryan Copley <github@ryancopley.com>
2015-05-06 14:02:32 -0400

    Async stuff

--- Async/AppDelegate.m
+++ Async/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Async.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Async runtest];
+    
     return YES;
 }
 
---
+++ Async/Async.h
@@ -0,0 +1,18 @@
+//
+//  Async.h
+//  Async
+//
+//  Created by Ryan Copley on 5/6/15.
+//  Copyright (c) 2015 Ryan Copley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Async : NSObject
+
++(void)waterfall:(NSArray*)calls error:(void (^)())error success:(void (^)())success;
++(void)forEach:(NSArray*)array call:(void (^)(id obj, void (^cb)()))eachCall error:(void (^)())error success:(void (^)())success;
+
++(void)runtest;
+
+@end
