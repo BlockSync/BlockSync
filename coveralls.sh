ee3f032394c93749d261ceef4196c3a774ffccff
Ryan Copley <github@ryancopley.com>
2015-05-06 20:44:34 -0400

    Inspecting the FS of travis... my gnco files should appear :(

--- coveralls.sh
+++ coveralls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ls -las "${DIR_BUILD}"
 source xcenv.sh
 declare -r DIR_BUILD="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
 ./xcode-coveralls --verbose --exclude "Example" --token GSk2EukcT1Fh84ii91H9p7pVPu9GehqAc "${DIR_BUILD}"
