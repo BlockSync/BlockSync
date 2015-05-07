b3c27e54a3827fcca180664553c06dd5e2af4c20
Ryan Copley <github@ryancopley.com>
2015-05-06 20:39:53 -0400

    Lets see if this works

--- coveralls.sh
+++ coveralls.sh
@@ -2,4 +2,4 @@
 
 source xcenv.sh
 declare -r DIR_BUILD="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
-./xcode-coveralls "${DIR_BUILD}"
+./xcode-coveralls --verbose --exclude "Example" --token GSk2EukcT1Fh84ii91H9p7pVPu9GehqAc "${DIR_BUILD}"
